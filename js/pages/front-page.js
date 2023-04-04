6802eabc99df86b50ecb4acefc82bba47b266635
yoshuu <appcs342@gmail.com>
2023-04-04 19:41:35 +0800

    clear code

--- js/pages/front-page.js
+++
@@ -1,6 +0,0 @@
-// const about = document.querySelector(".candidate-about-me-button");
-// const mobilePopuping = document.querySelector(".mobile-popuping");
-
-// about.addEventListener("click", function (e) {
-//   mobilePopuping.showModal();
-// });
